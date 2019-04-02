b0ab9d5ef1089dc0e113c584a22c8ee9dc244c41
Mason Salcido <MvSvlc@nmsu.edu>
2019-04-02 10:08:24 -0600

    Added remaining pages to solution and organized all user controls

--- CharacterCreator/UserControls/Page4Control.cs
+++ CharacterCreator/UserControls/Page4Control.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CharacterCreator
 {
-    public partial class Form2 : Form
+    public partial class Page4Control : UserControl
     {
-        public Form2()
+        public Page4Control()
         {
             InitializeComponent();
         }
