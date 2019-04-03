f8205a191e310272dc6bf63f6436b8b2a2da81c7
David Torres <davidt17@nmsu.edu>
2019-04-03 15:29:03 -0600

    Added Select Class dropDownList and Labels for Race and Class, and reformated the overall organization of page 2

--- CharacterCreator/UserControls/Page2Control.Designer.cs
+++ CharacterCreator/UserControls/Page2Control.Designer.cs
@@ -32,12 +32,17 @@
             this.textBoxName = new System.Windows.Forms.TextBox();
             this.comboBoxRace = new System.Windows.Forms.ComboBox();
             this.labelRace = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelName = new System.Windows.Forms.Label();
+            this.comboBoxClass = new System.Windows.Forms.ComboBox();
+            this.labelClass = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.label1.Location = new System.Drawing.Point(258, 0);
+            this.label1.Location = new System.Drawing.Point(327, 3);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(210, 25);
             this.label1.TabIndex = 0;
@@ -47,7 +52,7 @@
             // 
             this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBoxName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(188)))), ((int)(((byte)(156)))));
-            this.textBoxName.Location = new System.Drawing.Point(263, 107);
+            this.textBoxName.Location = new System.Drawing.Point(75, 3);
             this.textBoxName.Name = "textBoxName";
             this.textBoxName.Size = new System.Drawing.Size(205, 20);
             this.textBoxName.TabIndex = 1;
@@ -58,7 +63,7 @@
             // comboBoxRace
             // 
             this.comboBoxRace.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxRace.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxRace.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBoxRace.ForeColor = System.Drawing.Color.Black;
             this.comboBoxRace.FormattingEnabled = true;
             this.comboBoxRace.Items.AddRange(new object[] {
@@ -66,7 +71,7 @@
             "Dwarf",
             "Elf",
             "Halfing"});
-            this.comboBoxRace.Location = new System.Drawing.Point(263, 169);
+            this.comboBoxRace.Location = new System.Drawing.Point(75, 115);
             this.comboBoxRace.Name = "comboBoxRace";
             this.comboBoxRace.Size = new System.Drawing.Size(205, 21);
             this.comboBoxRace.TabIndex = 2;
@@ -74,26 +79,85 @@
             // 
             // labelRace
             // 
+            this.labelRace.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.labelRace.AutoSize = true;
             this.labelRace.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(188)))), ((int)(((byte)(156)))));
-            this.labelRace.Location = new System.Drawing.Point(165, 169);
+            this.labelRace.Location = new System.Drawing.Point(3, 112);
             this.labelRace.Name = "labelRace";
             this.labelRace.Size = new System.Drawing.Size(66, 13);
             this.labelRace.TabIndex = 3;
             this.labelRace.Text = "Select Race";
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.labelClass, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelName, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxRace, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxName, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelRace, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxClass, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 31);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 73.68421F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.31579F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 119F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(374, 272);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // labelName
+            // 
+            this.labelName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelName.AutoSize = true;
+            this.labelName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(188)))), ((int)(((byte)(156)))));
+            this.labelName.Location = new System.Drawing.Point(18, 0);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 4;
+            this.labelName.Text = "Name";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBoxClass
+            // 
+            this.comboBoxClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxClass.ForeColor = System.Drawing.Color.Black;
+            this.comboBoxClass.FormattingEnabled = true;
+            this.comboBoxClass.Items.AddRange(new object[] {
+            "Cleric",
+            "Fighter",
+            "Rogue",
+            "Wizard"});
+            this.comboBoxClass.Location = new System.Drawing.Point(75, 155);
+            this.comboBoxClass.Name = "comboBoxClass";
+            this.comboBoxClass.Size = new System.Drawing.Size(205, 21);
+            this.comboBoxClass.TabIndex = 5;
+            // 
+            // labelClass
+            // 
+            this.labelClass.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelClass.AutoSize = true;
+            this.labelClass.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(188)))), ((int)(((byte)(156)))));
+            this.labelClass.Location = new System.Drawing.Point(3, 152);
+            this.labelClass.Name = "labelClass";
+            this.labelClass.Size = new System.Drawing.Size(65, 13);
+            this.labelClass.TabIndex = 6;
+            this.labelClass.Text = "Select Class";
+            // 
             // Page2Control
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.Controls.Add(this.labelRace);
-            this.Controls.Add(this.comboBoxRace);
-            this.Controls.Add(this.textBoxName);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Page2Control";
             this.Size = new System.Drawing.Size(757, 479);
             this.Load += new System.EventHandler(this.Page2Control_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -103,5 +167,9 @@
         private System.Windows.Forms.TextBox textBoxName;
         private System.Windows.Forms.ComboBox comboBoxRace;
         private System.Windows.Forms.Label labelRace;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.ComboBox comboBoxClass;
+        private System.Windows.Forms.Label labelClass;
     }
 }
