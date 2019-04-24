3b0ab4cab4a91870579bcd42f77c49dbbe6f5278
Mason Salcido <MvSvlc@nmsu.edu>
2019-04-23 18:04:07 -0600

    Aligned ListBox on HomeControl to center

--- CharacterCreator/UserControls/HomeControl.Designer.cs
+++ CharacterCreator/UserControls/HomeControl.Designer.cs
@@ -29,20 +29,30 @@
         private void InitializeComponent()
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(101, 62);
+            this.listBox1.Location = new System.Drawing.Point(107, 61);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(541, 355);
             this.listBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(757, 479);
+            this.panel1.TabIndex = 2;
             // 
             // HomeControl
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
             this.Name = "HomeControl";
             this.Size = new System.Drawing.Size(757, 479);
             this.ResumeLayout(false);
@@ -52,5 +62,6 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
