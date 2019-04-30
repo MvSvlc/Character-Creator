7b0583cf0cc049d019da975ee83179a46878f20e
David Torres <davidt17@nmsu.edu>
2019-04-30 01:33:30 -0600

    Corrected the text color on the character details in the home page.

--- CharacterCreator/UserControls/HomeControl.cs
+++ CharacterCreator/UserControls/HomeControl.cs
@@ -21,6 +21,7 @@ namespace CharacterCreator
         {
             Builder.initCharFiles();
             Character char1 = Utils.BinaryDeserialize(Builder.CharFile1);
+            listView1.ForeColor = Color.FromArgb(26, 188, 156);
             listView1.Items.Add(char1.Name).SubItems.AddRange(new string[] { char1.Race, char1.Subrace, char1.Class, char1.Background }, Color.FromArgb(26, 188, 156), Color.FromArgb(34, 52, 70), DefaultFont);
         }
 
@@ -28,7 +29,8 @@ namespace CharacterCreator
         {
             Character char1 = Utils.BinaryDeserialize(Builder.CharFile1);
             listView1.Items.Clear();
-            listView1.Items.Add(char1.Name).SubItems.AddRange(new string[]{ char1.Race, char1.Subrace, char1.Class, char1.Background }, Color.FromArgb(26, 188, 156), Color.FromArgb(34, 52, 70), DefaultFont);
+            listView1.ForeColor = Color.FromArgb(26, 188, 156);
+            listView1.Items.Add(char1.Name).SubItems.AddRange(new string[]{ char1.Race, char1.Subrace, char1.Class, char1.Background }, Color.FromArgb(26, 188, 156), Color.FromArgb(34, 52, 70), new Font(DefaultFont, FontStyle.Regular));
         }
     }
 }
